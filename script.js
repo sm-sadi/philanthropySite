ef99d156a3a4eaf60956b7d610657a612e18864e
sm-sadi <smsadi19@gmail.com>
2025-08-17 14:45:23 +0200

    itial

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    document.getElementById('formMessage').textContent = 'Thank you! Your message has been sent.';
+    this.reset();
+});
