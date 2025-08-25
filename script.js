724e03f81464afae516f90ac9837f0a5a3bbf2e9
sm-sadi <smsadi19@gmail.com>
2025-08-25 13:25:12 +0200

    added error message on form submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.getElementById('contactForm').addEventListener('submit', function(e) {
     e.preventDefault();
-    document.getElementById('formMessage').textContent = 'Thank you! Your message has been sent.';
+    document.getElementById('formMessage').textContent = 'Message can not be sent due to error, Please reachout to: smsadi19@gmail.com';
+    alert("Message can not be sent due to error, Please reachout to: smsadi19@gmail.com")
     this.reset();
 });
